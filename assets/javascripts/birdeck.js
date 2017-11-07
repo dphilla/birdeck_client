a3d47bb97e6641ee90a03c790249a7b90c92445d
Daniel Phillips <danieledward.phillips@gmail.com>
2017-11-07 13:04:49 -0700

    Add in-class walkthrough

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -1,4 +1,7 @@
-var API;
+
+// in refactoring: import 'ajax-request.js'
+
+var API = "http://localhost:3000";
 
 $(document).ready(function(){
   // #tabs for CRUD actions activated
@@ -8,4 +11,165 @@ $(document).ready(function(){
   $('form').on('submit', function(event){
     event.preventDefault();
   });
+
+  var getAllPosts = function() {
+    return $.ajax({
+      url: API + '/api/v1/posts',
+      method: 'GET',
+    }).done(function(data) {
+      console.log(data);
+      for(i = 0; data.length; i++) {
+        $('#latest-posts').append('<p class="post">' + data[i].description + '</p>');
+      }
+    }).fail(function() {
+      handleError();
+    })
+  }
+
+  var getSinglePost = function() {
+    var postId = $(".show_form input[name='show-id']").val();
+    return $.ajax({
+      url: API + '/api/v1/posts/' + postId,
+      method: 'GET',
+    }).done(function(data) {
+        $('#latest-posts').append('<p class="post">' + data[i].description + '</p>');
+    }).fail(function() {
+      handleError();
+    })
+  }
+
+   var createNewPost = function( ){
+    var postDesciption =  $(".post-form input[name='post-description']").val();
+    return $.ajax({
+      url: API + '/api/v1/posts',
+      method: 'POST',
+      data: { post: {description: postDesciption}}
+    }).done(function(data) {
+      console.log(data);
+      for(i = 0; data.length; i++) {
+        $('#latest-posts').append('<p class="post">' + data[i].description + '</p>');
+      }
+    }).fail(function() {
+      handleError();
+    })
+  }
+
+  var updatePost = function(){
+    var postDesciption =  $(".update-form input[name='post-description']").val();
+    var postId =  $(".update-form input[name='update-id']").val();
+    return $.ajax({
+      url: API + '/api/v1/posts' + postId,
+      method: 'PUT',
+      data: { post: {description: postDesciption}}
+    }).done(function(data) {
+      console.log(data);
+      for(i = 0; data.length; i++) {
+        $('#latest-posts').append('<p class="post">' + data[i].description + '</p>');
+      }
+    }).fail(function() {
+      handleError();
+    })
+  }
+
+    var handleError = function() {
+      $('#latest-posts').append('<p class="post">Something went wrong. Try again later</p>');
+    }
+
+  $('button[name="button-fetch"]').on('click', getAllPosts);
+  $(".show-form input[type='submit']").on('click', getSinglePost);
+  $(".post-form input[type='submit']").on('click', createNewPost);
+  $(".update-form input[type='submit']").on('click', updatePost);
+
 });
+
+// Cory Walkthrough
+
+
+//var API = "http://localhost:3000";
+
+//$(document).ready(function(){
+  //$( "#tabs" ).tabs();
+
+  //$('form').on('submit', function(event){
+    //event.preventDefault();
+  //});
+
+  //var getAllPosts = function() {
+    //return $.ajax({
+      //url: API + '/api/v1/posts',
+      //method: 'GET',
+    //}).done(function(data) {
+      //console.log(data);
+      //for(i = 0; data.length; i++) {
+        //$('#latest-posts').append('<p class="post">' + data[i].description + '</p>');
+      //}
+    //}).fail(function() {
+      //handleEror();
+    //})
+  //}
+
+  //var getSinglePost = function() {
+    //var postId = $(".show-form input[name='show-id']").val();
+
+    //return $.ajax({
+      //url: API + '/api/v1/posts/' + postId,
+      //method: 'GET',
+    //}).done(function(data) {
+      //$('#latest-posts').append('<p class="post">' + data.description + '</p>');
+    //}).fail(function() {
+      //handleEror();
+    //})
+  //}
+
+  //var createNewPost = function() {
+    //var postDescription = $(".post-form input[name='post-description']").val();
+
+    //return $.ajax({
+      //url: API + '/api/v1/posts',
+      //method: 'POST',
+      //data: { post: { description: postDescription } }
+    //}).done(function(data) {
+      //$('#latest-posts').append('<p class="post">New post has been created!</p>');
+    //}).fail(function() {
+      //handleEror();
+    //})
+  //}
+
+  //var updatePost = function() {
+    //var postId = $(".update-form input[name='update-id']").val();
+    //var postDescription = $(".update-form input[name='post-description']").val();
+
+    //return $.ajax({
+      //url: API + '/api/v1/posts/' + postId,
+      //method: 'PUT',
+      //data: { post: { description: postDescription } }
+    //}).done(function(data) {
+      //$('#latest-posts').append('<p class="post">New post has been updated!</p>');
+    //}).fail(function() {
+      //handleEror();
+    //})
+  //}
+
+  //var deletePost = function() {
+    //var postId = $(".delete-form input[name='delete-id']").val();
+
+    //return $.ajax({
+      //url: API + '/api/v1/posts/' + postId,
+      //method: 'DELETE',
+    //}).done(function(data) {
+      //$('#latest-posts').append('<p class="post">This post has been deleted</p>');
+    //}).fail(function() {
+      //handleEror();
+    //})
+  //}
+
+  //var handleEror = function() {
+    //$('#latest-posts').append('<p class="post">Something went wrong. Try again later</p>');
+  //}
+
+  //$('button[name="button-fetch"]').on('click', getAllPosts);
+  //$(".show-form input[type='submit']").on('click', getSinglePost);
+  //$('.post-form input[type="submit"]').on('click', createNewPost);
+  //$('.update-form input[type="submit"]').on('click', updatePost);
+  //$('.delete-form input[type="submit"]').on('click', deletePost);
+//});
